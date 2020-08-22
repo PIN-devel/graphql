8d644e8b1a1a5ffffbe3a4309d58dadf82fc4d91
PIN-devel <pin954562@gmail.com>
2020-08-23 02:15:11 +0900

    add vuex & set webpack

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'frontend/dist'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+VUE_DIR = os.path.join(BASE_DIR, 'frontend')
+STATICFILES_DIRS = (
+    os.path.join(VUE_DIR, 'dist','static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-]
+    path('', TemplateView.as_view(template_name='index.html'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
