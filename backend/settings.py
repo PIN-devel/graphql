c67852baeeeaf82efb7f8ce92be603856693e28b
PIN-devel <pin954562@gmail.com>
2020-09-08 23:18:26 +0900

    add dockerfiles for django

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '9_5y7zyo5%bz$0m&65jt+c4mjpvj+yjtze0f7cm$iymbxon*5u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
